b4cb4f581b3b44043db162fd29c1ac44d43aa227
Gage Rowden <rowde002@ad.ahc.umn.edu>
2024-05-16 16:41:47 -0500

    Added statistical analysis and some graphs for MPR, MS, RAF, and TtT.

--- calculate_metrics.R
+++ calculate_metrics.R
@@ -1,19 +1,29 @@
 library(slider)
+library(ggpubr)
+library(agricolae)
+library(PMCMRplus)
 
 
 
+source("C:/Users/rowde002/Box/Scripts/R scripts/functions/functions.R")
 
-source("functions/functions.R")
 
 
 # For testing, use "test".
-file <- paste0(
-  "input files/",
-  readline(
-    "Please input the Excel file name without .xlsx extension: "
+file <- ""
+while (file == "") {
+  file <- paste0(
+    readline(
+      "Please input the Excel file name without .xlsx extension: "
     ),
-  ".xlsx"
+    ".xlsx"
   )
+  if (!file.exists(file)) {
+    print(paste(file, "does not exist. Please enter a valid file name."))
+    file <- ""
+  }
+}
+
 
 df <- get_real(file, ordered=FALSE)
 
@@ -147,12 +157,20 @@ for (i in 1: nrow(df_deriv)) {
 ################################################################################
 
 df_analyzed <- df_analyzed[order(df_analyzed$Sample_ID),]
+
+# Reorganize the columns of df_analyzed.
+df_analyzed <- data.frame(Sample_ID = df_analyzed$Sample_ID,
+                          MPR = df_analyzed$MPR,
+                          RAF = df_analyzed$RAF,
+                          MS  = df_analyzed$MS,
+                          TtT = df_analyzed$TtT)
+
 summary <- df_analyzed %>%
   group_by(Sample_ID) %>%
-  summarise(mean_TtT = mean(TtT), 
-            mean_RAF = mean(RAF), 
-            mean_MPR = mean(MPR), 
-            mean_MS = mean(MS))
+  summarise(mean_TtT   = mean(TtT), 
+            mean_RAF   = mean(RAF),
+            mean_MPR   = mean(MPR),
+            mean_MS    = mean(MS))
 
 ################################################################################
 
@@ -169,9 +187,57 @@ writeData(wb, "Summary", summary)
 
 
 # Save the Excel file.
+saveWorkbook(wb, "output.xlsx", overwrite = TRUE)
 
-saveWorkbook(wb, "output files/output.xlsx", overwrite = TRUE)
+################################################################################
 
+# Calculate the statistical comparisons for MPR.
+model <- aov(MPR ~ Sample_ID, data=df_analyzed)
+stats <- LSD.test(model, "Sample_ID", p.adj = "bonferroni")
+
+stats <- stats$groups[order(row.names(stats$groups)),]
+neg_group <- stats["N", "groups"]
+  
+summary$MPR_Result <- c(ifelse(stats$groups == neg_group, "ns", "*"))
+
+
+# Calculate the statistical comparisons for Max Slope.
+model <- aov(MS ~ Sample_ID, data=df_analyzed)
+stats <- LSD.test(model, "Sample_ID")
+
+stats <- stats$groups[order(row.names(stats$groups)),]
+neg_group <- stats["N", "groups"]
+
+summary$MS_Result <- c(ifelse(stats$groups == neg_group, "ns", "*"))
+
+################################################################################
+
+# Plot the summary metrics
+df_analyzed_melted <- reshape2::melt(df_analyzed, id.vars = "Sample_ID")
+p <- ggplot(df_analyzed_melted, aes(Sample_ID, value, fill=Sample_ID)) +
+  scale_fill_discrete() +
+  geom_boxplot() +
+  # stat_compare_means() +
+  facet_wrap(vars(variable),
+             scales = "free",
+             labeller = as_labeller(c(MPR = "MPR (Max RFU / Initial RFU)",
+                                      RAF = "RAF (1/s)",
+                                      MS = "Max Slope (RFU/s)",
+                                      TtT = "Time to Threshold (h)")),
+             strip.position = "left") +
+  xlab(NULL) +
+  ylab(NULL) +
+  theme(axis.line = element_line(colour = "black"),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.background = element_blank(),
+        panel.grid = element_line(colour = "lightgrey"),
+        panel.border = element_rect(colour = "black", fill=NA, size=1),
+        legend.position = "none",
+        strip.background = element_blank(),
+        strip.placement = "outside")
+p
+
+ggsave("Summary.png", p, width = 3600, height = 2400, units = "px")
 
 
 
