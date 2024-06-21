56aceb709040aa0be25f4c46286b270510760927
gage1145 <140093724+gage1145@users.noreply.github.com>
2024-06-21 11:02:19 -0500

    Update BMG_format.R
    Added some comments that explain the script;
    changed the source function path.

--- BMG_format.R
+++ BMG_format.R
@@ -1,4 +1,8 @@
-source("C:/Users/rowde002/Box/Scripts/R scripts/functions/functions.R")
+# This script takes an .csv file plate layout and formats it into a .txt file that can be imported into the BMG control software.
+# See "formatted.txt" in the BMG formatting folder for the output example.
+# See "plate_layout.csv" in the BMG formatting folder for input file example.
+
+source("functions/functions.R")
 
 df <- BMG_format("plate_layout.csv")
 
