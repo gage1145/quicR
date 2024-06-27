572dee0c77c4f676168cb0f0a8f2d1f443460dad
Gage Rowden <rowde002@ad.ahc.umn.edu>
2024-06-27 09:21:20 -0500

    Changed write.table to writeLines. Refactored df to df_. Changed function source.

--- BMG_format.R
+++ BMG_format.R
@@ -1,9 +1,5 @@
-source("C:/Users/rowde002/Box/Scripts/R scripts/functions/functions.R")
+source("functions/functions.R")
 
-df <- BMG_format("plate_layout.csv")
+df_ <- BMG_format("BMG formatting/plate_layout.csv")
 
-write.table(df,
-            file="formatted.txt", 
-            quote=FALSE, sep=" ", 
-            col.names = FALSE, 
-            row.names=FALSE)
+writeLines(df_, "formatted.txt")
