8c16f01503ae75d91ce7b36aed5c0eff623269c2
Gage Rowden <gage.rowden1145@gmail.com>
2024-08-23 19:41:46 -0500

    Added testing files for organize_tables.

---
+++ tests/testthat/test_organize_tables.R
@@ -0,0 +1,35 @@
+library(testthat)
+library(usethis)
+library(quicR)
+
+
+use_r("organize_tables")
+
+files <- list.files("input files", full.names = TRUE)
+print(files)
+
+
+
+for (i in c("test", "test2", "test3")) {
+
+  file <- paste0("input files/", i, ".xlsx")
+
+  test_that(
+    "organize_tables returns list?",
+    {expect_type(organize_tables(file), "list")}
+  )
+
+  test_that(
+    "organize_tables returns list of tibbles?",
+    {expect_true(
+      class(organize_tables(file)[[1]])[1] == "tbl_df" &
+        class(organize_tables(file)[[1]])[2] == "tbl" &
+        class(organize_tables(file)[[1]])[3] == "data.frame"
+    )}
+  )
+}
+
+test_that(
+  "organize_tables accepts 384 as plate arg?",
+  {expect_no_error(organize_tables("input files/test384.xlsx", plate=384))}
+)
