12b7d517f3f7c1013de71528f3bacc21561315ae
unknown <gage.rowden1145@gmail.com>
2024-09-23 13:19:02 -0500

    Added the function "convert_tables" for formatting tables into dataframes.

--- plate_view_export.R
+++ plate_view_export.R
@@ -1,17 +1,20 @@
 library(readxl)
-library(tidyverse)
-library(ggpubr)
+library(dplyr)
 library(ggplot2)
-library(gridExtra)
 library(quicR)
 
 
-# Initialize parameters for downstream functions.###############################
+
+# Initialize parameters for downstream functions. -------------------------
+
+
 
 file <- ""
 while (file == "") {
-  file <- paste0(readline("Please input .xlsx file name WITHOUT extension: "),
-                 ".xlsx")
+  file <- paste0(
+    readline("Please input .xlsx file name WITHOUT extension: "),
+    ".xlsx"
+  )
   if (!(file.exists(file))) {
     print("File does not exist. Please ensure file name was typed correctly")
     file <- ""
@@ -20,47 +23,50 @@ while (file == "") {
 
 plate <- ""
 while (plate == "") {
-  plate <-  as.integer(readline("Please enter 96 or 384 for plate type: "))
+  plate <- as.integer(readline("Please enter 96 or 384 for plate type: "))
   if (plate != 96 & plate != 384) {
     plate <- ""
   }
 }
 
-# Following block exports the plate view figure.################################
+
+
+# Following block exports the plate view figure. --------------------------
+
+
 
 # Define the layout using the first sheet in the excel file.
 # The sheet should be formatted so that each ID in the "layout" table is unique.
-# "organize_tables" is sourced from plate_view_function.R.
-df_dic <- organize_tables(file, plate = plate)
-IDs <- t(df_dic[["Sample IDs"]])
-ID_list <- list()
-for (i in IDs) {
-  for (j in i) {
-    if (!(is.na(j))) {
-      ID_list <- append(ID_list, j)
-    }
-  }
-}
+df_dic <- quicR::organize_tables(file, plate = plate)
+
+# IDs <- df_dic[["Sample IDs"]] |>
+#   t() |>
+#   as.data.frame() |>
+#   tidyr::gather() |>
+#   dplyr::select(value)
+
+IDs <- convert_tables(df_dic)$`Sample IDs`
 
 # Determine if there is a dilutions table.
 dilution_bool <- "Dilutions" %in% names(df_dic)
 
 # Add dilution factors if applicable.
 if (dilution_bool) {
-  dilutions <- c()
-  for (i in t(df_dic[["Dilutions"]])) {
-    for (j in i) {
-      if (!is.na(j)) {
-        dilutions <- rbind(dilutions, j)
-      }
-    }
-  }
+  # dilutions <- df_dic[["Dilutions"]] |>
+  #   t() |>
+  #   as.data.frame() |>
+  #   tidyr::gather() |>
+  #   dplyr::select(value) |>
+  #   dplyr::mutate(value = -log10(as.numeric(value)))
+  dilutions <- convert_tables(df_dic)$Dilutions |>
+    as.numeric() |>
+    log10() * -1
 }
 
 # Read in the real-time data.
-# "get_real" will return a list of dataframes depending on how many real-time
+# get_real will return a list of dataframes depending on how many real-time
 # measurements the user exported from MARS.
-df_list <- get_real(file, ordered=FALSE)
+df_list <- quicR::get_real(file, ordered = FALSE)
 
 df_id <- as.integer(
   readline(
@@ -72,32 +78,33 @@ df_id <- as.integer(
   )
 )
 
-df <- data.frame(df_list[[df_id]])
-# df_meta <- get_meta(file)
+df <- as.data.frame(df_list[[df_id]])
 
-# Specify the time column.
-time_col <- df[, 1]
+# Set the time column as the df index.
+rownames(df) <- df[, 1]
 
 # Remove the time column and ID row.
 df <- df[, -1]
 
-# Set the time column as the df index.
-rownames(df) <- time_col
-
 # Get the wells used in the run.
-wells <- get_wells(file)
+wells <- quicR::get_wells(file)
 
 # Take the metadata and apply it into a dataframe for the plate_view function.
-sample_locations <- na.omit(do.call(rbind, Map(data.frame, A=wells, B=ID_list)))
+sample_locations <- cbind(wells, IDs) |> na.omit()
 
 # Add the dilutions if applicable.
 if (dilution_bool) {
-  sample_locations <- sample_locations %>%
-    mutate(Dilutions = -log10(as.numeric(dilutions))) %>%
-    unite(B, B:Dilutions, sep="\n")
+  sample_locations <- sample_locations |>
+    dplyr::mutate(Dilutions = dilutions$value) |>
+    tidyr::unite(value, value:Dilutions, sep = "\n")
 }
 
-# Run plate_view function which produces a plate view figure.
+
+
+# Run plate_view function which produces a plate view figure. -------------
+
+
+
 quicR::plate_view(df, sample_locations, plate)
 
 ggsave("plate_view.png", width = 3600, height = 2400, units = "px")
