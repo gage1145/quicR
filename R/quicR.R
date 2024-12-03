56aab9cc3d9a303c5a8ed349e63bdd6de187054f
Gage Rowden <gager@priogen.bio>
2024-12-03 16:30:38 -0600

    Added a documentation file that fixes the NOTE when using "." in a pipe.

---
+++ R/quicR.R
@@ -0,0 +1,14 @@
+#'  \code{quicR} package
+#'
+#'  Data handling for real-time quaking induced conversion assays
+#'
+#'  See the README on
+#'  \href{https://cran.r-project.org/web/packages/quicR/index.html}{CRAN}
+#'  or \href{https://github.com/gage1145/quicR}{Github}
+#'
+#'  @docType package
+#'  @name quicR
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1") utils::globalVariables(c("."))
