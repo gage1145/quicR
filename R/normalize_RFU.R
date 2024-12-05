546014b563fa1c26ee1ff62ba217b8f50217dde3
gage1145 <gage.rowden1145@gmail.com>
2024-12-05 11:36:36 -0600

    Split the transposition sub-function in normalize_RFU into a separate function named "transpose_real".

--- R/normalize_RFU.R
+++ R/normalize_RFU.R
@@ -9,59 +9,39 @@
 #'
 #' @return A dataframe containing real-time normalized fluorescence values.
 #'
-#' @importFrom dplyr rename
-#' @importFrom dplyr mutate_at
-#' @importFrom dplyr %>%
-#' @importFrom janitor row_to_names
-#'
 #' @examples
 #' # This test takes >5 sec
 #' \donttest{
-#' file <- system.file(
-#'   "extdata/input_files",
-#'   file = "test.xlsx",
-#'   package = "quicR"
-#' )
-#' df_ <- get_real(file)[[1]]
+#'   file <- system.file(
+#'     "extdata/input_files",
+#'     file = "test2.xlsx",
+#'     package = "quicR"
+#'   )
+#'   df_ <- get_real(file)[[1]]
 #'
-#' # Export the tables in the first sheet of the file.
-#' dic <- quicR::organize_tables(file)
+#'   # Export the tables in the first sheet of the file.
+#'   dic <- quicR::organize_tables(file)
 #'
-#' # Apply the column names.
-#' colnames(df_) <- cbind("Time", convert_tables(dic)$`Sample IDs` %>% t())
+#'   # Apply the column names.
+#'   sample_ids <- convert_tables(dic)$`Sample IDs`
+#'   colnames(df_) <- c("Time", sample_ids)
 #'
-#' # Normalize the raw data against the background reading.
-#' normalize_RFU(df_)
+#'   # Transpose the table
+#'   df_ <- quicR::transpose_real(df_)
+#'
+#'   # Normalize the raw data against the background reading.
+#'   normalize_RFU(df_)
 #' }
 #'
 #'
 #' @export
 normalize_RFU <- function(data, bg_cycle = 4) {
-
-  curate <- function(x) {
-
-    colnames(x) %>%
-      rbind(x) %>%
-      unname() %>%
-      t() %>%
-      as.data.frame() %>%
-      row_to_names(1) %>%
-      rename("Sample IDs" = "Time") %>%
-      mutate_at(-c(1), function(y) as.numeric(as.character(y)))
-  }
-
-  normalize <- function(x) {
-
-    df_norm <- x
-    for (i in 1:nrow(x)) {
-      for (j in 2:ncol(x)) {
+    df_norm <- data
+    for (i in 1:nrow(data)) {
+      for (j in 2:ncol(data)) {
         raw_value <- df_norm[i, j]
-        df_norm[i, j] <- raw_value / x[i, bg_cycle + 1]
+        df_norm[i, j] <- raw_value / data[i, bg_cycle + 1]
       }
     }
-
-    return(df_norm)
-  }
-
-  return(normalize(curate(data)))
+  return(df_norm)
 }
