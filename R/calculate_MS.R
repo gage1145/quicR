8f05c92405e1912176e75854a4c0536aec5d01f0
gage1145 <gage.rowden1145@gmail.com>
2025-01-02 18:10:16 -0600

    Formatted calculate_MS

--- R/calculate_MS.R
+++ R/calculate_MS.R
@@ -4,7 +4,7 @@
 #'
 #' @param data A dataframe containing real-time reads. It is recommended to use a dataframe made from normalize_RFU.
 #' @param window Integer designating how wide you want the sliding window to be for calculating the moving average slope.
-#' @param data_is_norm Logical, if the data has not been normalized, will make a call to normalize_RFU.
+#' @param data_is_norm Logical; if FALSE, will make a call to normalize_RFU.
 #'
 #' @return A dataframe containing the real-time slope values.
 #'
@@ -31,9 +31,7 @@
 calculate_MS <- function(data, window = 3, data_is_norm = TRUE) {
   curate <- function(x) {
     x %>%
-      {
-        if (data_is_norm) . else normalize_RFU(.)
-      } %>%
+      {if (data_is_norm) . else normalize_RFU(.)} %>%
       t() %>%
       as.data.frame() %>%
       mutate_all(~ as.numeric(as.character(.))) %>%
