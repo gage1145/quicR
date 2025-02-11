72ea2ba00f90649aa721ca93d99b28d77913ae6e
Gage Rowden <gage.rowden1145@gmail.com>
2025-02-11 09:57:35 -0600

    Added conditions to no facet if only one variable is passed. Also converted the ... arg to two separate args, sample_col and fill.

--- R/plot_metrics.R
+++ R/plot_metrics.R
@@ -3,7 +3,8 @@
 #' Generates a faceted figure of boxplots.
 #'
 #' @param data A dataframe containing the calculated metrics from the "calculate" family of quicR functions.
-#' @param ... A selection of columns to be used as faceting variables. Typically this includes the sample IDs and dilution factors.
+#' @param sample_col The name of the column containing the sample IDs.
+#' @param fill The column containing the fill aesthetic. Usually the dilutions column.
 #' @param dilution_bool Logical; should dilution factors be included in the plot?
 #' @param nrow Integer; number of rows to output in the plot.
 #' @param ncol Integer; number of columns to output in the plot.
@@ -21,44 +22,45 @@
 #'   package = "quicR"
 #' )
 #'
-#' raw <- quicR::get_real(file)[[1]] |>
+#' data <- quicR::get_real(file)[[1]] |>
 #'   quicR::normalize_RFU()
 #'
 #' meta <- quicR::organize_tables(file) |>
 #'   quicR::convert_tables()
 #'
-#' data.frame(
-#'   `Sample IDs` = meta$`Sample IDs`,
-#'   `Dilutions`  = meta$Dilutions,
-#'   check.names = FALSE
-#' ) |>
-#'   dplyr::mutate(
-#'     MPR = quicR::calculate_MPR(raw),
-#'     MS  = quicR::calculate_MS(raw),
-#'     TtT = quicR::calculate_TtT(raw, 2),
-#'     RAF = 1 / TtT
-#'   ) |>
-#'   plot_metrics("Sample IDs", "Dilutions")
+#' calculate_metrics(data, meta) |>
+#'   plot_metrics()
 #'
 #' @export
-plot_metrics <- function(data, ..., dilution_bool = TRUE, nrow = 2, ncol = 2) {
+plot_metrics <- function(data, sample_col = "Sample IDs", fill = "Dilutions", dilution_bool = TRUE, nrow = 2, ncol = 2) {
+
+  variables <- function() {
+    (data %>% gather("variable", "value", -c(sample_col, fill)))$variable %>%
+      unique() %>%
+      length()
+  }
 
   data %>%
-    gather("variable", "value", -c(...)) %>%
+    gather("variable", "value", -c(sample_col, fill)) %>%
     ggplot(
-      aes(!!sym(c(...)[1]),
-          .data$value,
-          fill = if (dilution_bool) as.factor(!!sym(c(...)[2])))
+      aes(!!sym(sample_col),
+        .data$value,
+        fill = if (dilution_bool) as.factor(!!sym(fill))
+      )
     ) +
     geom_boxplot(
       position = position_dodge2(preserve = "single")
     ) +
-    facet_wrap(~variable, scales = "free_y", nrow = nrow, ncol = ncol) +
-    labs(fill = if (dilution_bool) "Dilutions") +
+    {if (variables() > 1) {
+      facet_wrap(~variable, scales = "free_y", nrow = nrow, ncol = ncol)
+    }} +
+    labs(
+      fill = if (dilution_bool) "Dilutions"
+    ) +
     theme(
       legend.position = "bottom",
       strip.text = element_text(face = "bold"),
       axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
-      axis.title = element_blank()
-  )
+      axis.title = if(variables() > 1) element_blank() else element_text()
+    )
 }
