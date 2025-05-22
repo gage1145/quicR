894c94d402b5a2a525b425952d6fe43fc6186026
Gage Rowden <gage.rowden1145@gmail.com>
2025-05-22 16:51:39 -0500

    Added benchmarking for the rtquicR package as well.

--- inst/extdata/benchmarking.R
+++ inst/extdata/benchmarking.R
@@ -1,5 +1,6 @@
 library(quicR)
 library(QuICSeedR)
+library(rtquicR)
 library(dplyr)
 library(readxl)
 library(stringr)
@@ -50,12 +51,14 @@ quicseedr_results <- benchmark(
 
 bulk_analyze <- function(files) {
   calculate <- function(file) {
-    calculate_metrics(
-      get_real(file)[[1]] |>
-        normalize_RFU(),
-      organize_tables(file, ifelse(str_detect(file, "384"), 384, 96)) |>
-        convert_tables()
-    )
+    if (!str_detect(file, "384")) {
+      calculate_metrics(
+        get_real(file)[[1]] |>
+          normalize_RFU(),
+        organize_tables(file, ifelse(str_detect(file, "384"), 384, 96)) |>
+          convert_tables()
+      )
+    } else return()
   }
   sapply(files, calculate)
 }
@@ -77,9 +80,49 @@ quicr_results <- benchmark(
   get_real(raw),
   normalize_RFU(data),
   calculate_metrics(norm, meta),
+  calculate_MPR(norm),
+  calculate_TtT(norm, 2),
   plot_metrics(analyzed),
   get_sample_locations(file),
   plate_view(data, locations),
+  replications = 50,
+  columns = c("test", "elapsed", "replications")
+)
+
+
+
+# rtquicR -----------------------------------------------------------------
+
+
+
+files <- list.files("input_files/rtquicR benchmarking", "test", full.names = TRUE)
+file <- files[1]
+
+batch_rtqr <- function(files) {
+  analyze <- function(file) {
+    data <- load_quic_results(file, "raw_table", "All Cycles", 11, 2)
+    meta <- read_xlsx(file, sheet = 3)
+    plot_data <- signal_curve(data, meta)
+    lag_data <- calc_lag_phase(data, meta, cutoff = 48)
+    calc_AUC_sig(plot_data)
+    calc_mpr(plot_data)
+  }
+  sapply(files, analyze)
+}
+
+raw <- load_quic_results(file, "raw_table", "All Cycles", 11, 2)
+meta <- read_xlsx(file, sheet = 3)
+data <- signal_curve(raw, meta, "baseline_RFU_per_well", 4)
+# p <- plot_signal_curve(data)
+
+
+
+rtquicR_results <- benchmark(
+  load_quic_results(file, "raw_table", "All Cycles", 11, 2),
+  batch_rtqr(files),
+  calc_mpr(data),
+  calc_lag_phase(raw, meta, cutoff = 48),
+  plot_signal_curve(data),
   replications = 50,
   columns = c("test", "elapsed", "replications")
 )
@@ -92,7 +135,8 @@ quicr_results <- benchmark(
 
 quicseedr_results$package <- "QuICSeedR"
 quicr_results$package <- "quicR"
+rtquicR_results$package <- "rtquicR"
 
-results <- rbind(quicseedr_results, quicr_results)
+results <- rbind(quicseedr_results, quicr_results, rtquicR_results)
 results$time_per_test <- results$elapsed / results$replications
 write.csv(results, "results.csv")
