bc3061f07a0158e9edbc00c09a11e5c578b7748a
gage1145 <gage.rowden1145@gmail.com>
2025-09-08 18:06:30 -0500

    Update testing.R

--- inst/extdata/testing.R
+++ inst/extdata/testing.R
@@ -6,11 +6,5 @@ library(tidyr)
 file <- "input_files/test3.xlsx"
 raw <- get_quic(file, plate=96)
 analyzed <- calculate_metrics(raw, "Sample IDs", "Dilutions", "Wells")
-
-file <- "input_files/raw.csv"
-df <- read.csv(file, check.names = FALSE)
-  # mutate_at(c("Sample IDs", "Dilutions", "Wells"), as.factor)
-
-
-
-plate_view(df, plot_deriv = F)
+plate_view(raw, plot_deriv = F)
+plot_metrics(analyzed, "MPR", "MS", "TtT", "RAF", dilution_bool = FALSE)
