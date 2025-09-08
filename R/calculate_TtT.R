c4b622bea7f00bcbc801151f8c637937c9f39ef8
gage1145 <gage.rowden1145@gmail.com>
2025-09-08 11:15:42 -0500

    Added the ability to calculate RAF to the calculate_TtT function.

--- R/calculate_TtT.R
+++ R/calculate_TtT.R
@@ -7,6 +7,7 @@
 #' @param time Column containing your time values.
 #' @param values Column containing your fluorescence values.
 #' @param .by Grouping factor. Should typically be by individual wells.
+#' @param calc_raf Logical; Should rate of amyloid formation be calculated?
 #'
 #' @return A vector containing the times to threshold
 #'
@@ -15,6 +16,8 @@
 #' @importFrom dplyr mutate_at
 #' @importFrom dplyr summarize
 #' @importFrom dplyr select
+#' @importFrom dplyr sym
+#' @importFrom dplyr %>%
 #' @importFrom purrr is_empty
 #'
 #' @examples
@@ -27,22 +30,30 @@
 #'   calculate_TtT(threshold = 2)
 #'
 #' @export
-calculate_TtT <- function(data, threshold, time="Time", values="Norm", .by="Wells") {
+calculate_TtT <- function(data, threshold, time="Time", values="Norm", .by="Wells", calc_raf=TRUE) {
 
   dt <- data$Time[2] - data$Time[1]
+  grouping <- sym(.by)
 
   data %>%
-    group_by(!!sym(.by)) %>%
+    {if (is_grouped_df(.)) . else group_by(., !!grouping)} %>%
     rename("y" = !!sym(values), "x" = !!sym(time)) %>%
     mutate_at(c("x", "y"), as.numeric) %>%
     summarize(
-      "x2" = ifelse(!is_empty(.data$x[.data$y > 2]), min(.data$x[.data$y > 2]), max(.data$x)),
+      "x2" = ifelse(
+        !is_empty(.data$x[.data$y > 2]),
+        min(.data$x[.data$y > 2]),
+        max(.data$x)
+      ),
       "x1" = .data$x2 - dt,
       "y2" = .data$y[.data$x == .data$x2],
       "y1" = .data$y[.data$x == .data$x1],
       "TtT" = ifelse(
-        .data$x2 == max(.data$x), .data$x2, .data$x1 + (threshold - .data$y1) * (.data$x2 - .data$x1) / (.data$y2 - .data$y1)
+        .data$x2 == max(.data$x),
+        .data$x2,
+        .data$x1 + (threshold - .data$y1) * (.data$x2 - .data$x1) / (.data$y2 - .data$y1)
       )
     ) %>%
-    select(!!sym(.by), "TtT")
+    mutate(RAF = ifelse(calc_raf, 1/.data$TtT, NA)) %>%
+    select(!!sym(.by), "TtT", "RAF")
 }
