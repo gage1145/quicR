f56948afa621dfc002319702117eae3916d6612f
Gage Rowden <gage.rowden1145@gmail.com>
2025-09-24 13:23:31 -0500

    Added some styling options for plate_view and plot_metrics

--- R/plot_metrics.R
+++ R/plot_metrics.R
@@ -5,6 +5,7 @@
 #' @param data A dataframe containing the calculated metrics from the "calculate" family of quicR functions.
 #' @param ... A list of faceting columns.
 #' @param sample_col The name of the column containing the sample IDs.
+#' @param color The color used for the boxplot outline.
 #' @param fill The column containing the fill aesthetic. Usually the dilutions column.
 #' @param dilution_bool Logical; should dilution factors be included in the plot?
 #' @param nrow Integer; number of rows to output in the plot.
@@ -31,7 +32,7 @@
 #' }
 #'
 #' @export
-plot_metrics <- function(data, ..., sample_col = "Sample IDs", fill = "Dilutions", dilution_bool = TRUE, nrow = 2, ncol = 2) {
+plot_metrics <- function(data, ..., sample_col = "Sample IDs", color="black", fill = "Dilutions", dilution_bool = TRUE, nrow = 2, ncol = 2) {
 
   data %>%
     pivot_longer(c(...)) %>%
@@ -40,7 +41,7 @@ plot_metrics <- function(data, ..., sample_col = "Sample IDs", fill = "Dilutions
         fill = if (dilution_bool) as.factor(!!sym(fill))
       )
     ) +
-    geom_boxplot(position = position_dodge2(preserve = "single")) +
+    geom_boxplot(position = position_dodge2(preserve = "single"), color=color) +
     {if (length(c(...)) > 1) {
       facet_wrap(~.data$name, scales = "free_y", nrow = nrow, ncol = ncol)
     }} +
