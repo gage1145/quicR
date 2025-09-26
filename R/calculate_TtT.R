3c8aa4c5079fd8e5ec6643fce7efa13f4dd09a9a
gage1145 <gage.rowden1145@gmail.com>
2025-09-26 13:36:57 -0500

    Fixed a bug that prevented calculate_TtT from using the actual threshold.

--- R/calculate_TtT.R
+++ R/calculate_TtT.R
@@ -44,10 +44,9 @@ calculate_TtT <- function(data, threshold, time="Time", values="Norm", .by="Well
     rename("y" = !!sym(values), "x" = !!sym(time)) %>%
     mutate_at(c("x", "y"), as.numeric) %>%
     summarize(
-      crossed = max(.data$y) > threshold,
-      x2 = ifelse(
-        .data$crossed,
-        min(.data$x[.data$y>threshold]),
+      "x2" = ifelse(
+        !is_empty(.data$x[.data$y > threshold]),
+        min(.data$x[.data$y > threshold]),
         max(.data$x)
       ),
       x1 = .data$x2 - dt,
