9aa9360c4e6312a6d543705ee7a3b0a644e60bb2
gage1145 <gage.rowden1145@gmail.com>
2026-07-27 17:05:31 -0500

    Deprecated some arguments in get_quic.

--- R/get_quic.R
+++ R/get_quic.R
@@ -3,11 +3,12 @@
 #' Accepts an Excel file or a data frame of real-time RT-QuIC data.
 #'
 #' @param file An Excel file exported by BMG.
-#' @param transpose_table Logical, if true, will transpose the table(s). This should almost always be true.
+#' @param transpose_table `r lifecycle::badge("deprecated")` This argument no longer has any effect.
 #' @param norm_point Integer, defines the cycle to use as background fluorescence.
 #' @param which_table Integer, defines which table in the Excel sheet contains the real-time data. Should usually be set to 1.
 #' @param window_size Integer, defines the window size for estimating the derivative.
-#' @param .by Grouping factor. Should typically be by individual wells.
+#' @param .by `r lifecycle::badge("deprecated")` Use "by" instead.
+#' @param by Grouping factor. Should typically be by individual wells.
 #' @param plate Integer; either 96 or 384 to denote the type of well plate being used.
 #'
 #' @return A data frame containing all time-series data and sample/plate metadata.
@@ -24,13 +25,27 @@
 #' get_quic(file)
 #'
 #' @export
-get_quic <- function(file, transpose_table=TRUE, norm_point=2, which_table=1,
-                     window_size=2, .by="Wells", plate=96) {
+get_quic <- function(file, transpose_table=lifecycle::deprecated(), norm_point=2, which_table=1,
+                     window_size=2, smooth = FALSE, smooth_factor = 10, zero=FALSE, 
+                     .by=lifecycle::deprecated(), by=c("Well"), plate=96, sheet = 2) {
 
-  sheets <- lapply(1:2, function(x) suppressMessages(read_xlsx(file, sheet=x, col_names=FALSE)))
+  if (lifecycle::is_present(transpose_table)) {
+    lifecycle::deprecate_warn(
+      when = "3.0.8", 
+      what = "get_quic(transpose_table)"
+    )
+  }
 
-  meta <- organize_tables(sheets[[1]], plate=plate) %>%
-    convert_tables()
+  if (lifecycle::is_present(.by)) {
+    lifecycle::deprecate_warn(
+      when = "3.0.8", 
+      what = "get_quic(smooth)"
+    )
+    by <- .by
+  }
+  
+  data <- read_xlsx(file, sheet=sheet, col_names=FALSE) %>%
+    suppressMessages()
 
   get_real(sheets[[2]], transpose_table=transpose_table)[[which_table]] %>%
     mutate(
